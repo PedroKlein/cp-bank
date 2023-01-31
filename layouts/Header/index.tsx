9e95388537be2cc03a00821198e36985397790a6
Pedro Klein <pedroklein1@hotmail.com>
2023-01-31 19:30:27 -0300

    feat: initial db schema and basic auth flow

--- layouts/Header/index.tsx
+++ layouts/Header/index.tsx
@@ -1,10 +1,17 @@
+import { signOut, useSession } from "next-auth/react";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import React, { useState } from "react";
 import { MdNightlightRound, MdWbSunny } from "react-icons/md";
 import ThemeSwitch from "../../components/ThemeSwitch";
 import styles from "./header.module.scss";
 
 const Header: React.FC = () => {
+  const router = useRouter();
+  const { data: session, status } = useSession();
+  const isActive: (pathname: string) => boolean = (pathname) =>
+    router.pathname === pathname;
+
   return (
     <header className="container">
       <nav>
@@ -17,9 +24,18 @@ const Header: React.FC = () => {
         </ul>
 
         <ul>
-          <li>
-            <Link href="/">Home</Link>
-          </li>
+          {session ? (
+            <li>
+              <a onClick={() => signOut()}>Log out</a>
+            </li>
+          ) : (
+            <li>
+              <Link data-active={isActive("/signup")} href="/api/auth/signin">
+                Log in
+              </Link>
+            </li>
+          )}
+
           <li>
             <ThemeSwitch />
           </li>
