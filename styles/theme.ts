20f3a6d63bf7b260c6f84c4bf6bb29208ca7cb8d
Pedro Klein <pedroklein1@hotmail.com>
2023-02-15 21:07:52 -0300

    wip: fix anchor and set themes

--- styles/theme.ts
+++ styles/theme.ts
@@ -1,15 +1,14 @@
 import { createTheme } from "@mui/material/styles";
-import { green } from "@mui/material/colors";
 
 const theme = createTheme({
-  palette: {
-    primary: {
-      main: green.A700,
-    },
-    secondary: {
-      main: "#ff6666",
-    },
-  },
+  // palette: {
+  //   primary: {
+  //     main: green.A700,
+  //   },
+  //   secondary: {
+  //     main: "#ff6666",
+  //   },
+  // },
 });
 
 export default theme;
